edcbb3082e9ea9e520191abd3f3bb17c322676e8
Evgeny Chugaev <33101086+Evgeny-Chugaev@users.noreply.github.com>
2021-05-10 00:57:57 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Version 2.0</title>
+	<script src="src/assets/js/script.js"></script>
+	<link rel="stylesheet" href="src/assets/css/style.css">
+</head>
+<body>
+	<h1>Hello, git!</h1>
+</body>
+</html>
