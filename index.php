3be57742fee63b9950f36c13aa2dcc4f972c5e47
Evgeny-Chugaev <chugaev@artlebedev.ru>
2021-05-10 01:28:42 +0300

    fix assets path

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Version 1.0</title>
-	<script src="src/assets/js/script.js"></script>
-	<link rel="stylesheet" href="src/assets/css/style.css">
+	<script src="assets/script.js"></script>
+	<link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
 	<h1>Hello, git!</h1>
